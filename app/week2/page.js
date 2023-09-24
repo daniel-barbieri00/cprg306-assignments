848c60edf684f47e53c30db67d8a38d8d891bcc1
Daniel <daniel.barbieri@edu.sait.ca>
2023-09-24 12:30:20 -0600

    First Commit: Completed on Sept 24th for Assignment 2 on Page 2

---
+++ app/week2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "./studentInfo";
+
+export default function studentInfo() {
+    return (
+        <>
+            <h1>My Shopping List</h1>
+            <StudentInfo />
+        </>
+    )
+}
---
+++ app/week2/studentInfo.js
@@ -0,0 +1,11 @@
+export default function StudentInfo() {
+    return (
+        <>
+            <div>
+                <p>Name: Daniel Barbieri</p>
+                <p>Course Section: CPRG-306-E</p>
+                <p>https://github.com/daniel-barbieri00/cprg306-assignments</p>
+            </div>
+        </>
+    )
+}
